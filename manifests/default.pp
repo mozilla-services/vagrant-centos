484a1ab7c8172176ece26352c817c51a8e54dfb7
Ben Bangert <ben@groovie.org>
2012-03-28 12:40:33 -0700

    use yumrepo to manage repos

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,36 +3,21 @@ Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/us
 # Mozilla packages to install
 $moz_packages = ['nginx', 'gunicorn', 'logstash', 'python26', 'python26-setuptools', 'rubygem-petef-statsd', 'zeromq']
 
-# Add rpmforge
-exec { 'install-rpmforge-gpg-key':
-    command => 'rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt',
-    unless  => 'rpm -q --quiet gpg-pubkey-6b8d79e6',
+yumrepo { 'mozilla-services':
+    baseurl => "http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.mozilla-services",
+    enabled => 1,
+    gpgcheck    => 0,
 }
 
-exec { 'install-rpmforge':
-    command => 'rpm -i http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
-    unless  => 'rpm -q rpmforge-release',
-    require => Exec['install-rpmforge-gpg-key'],
+yumrepo { 'packages-mozilla':
+    baseurl => "http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.mozilla",
+    enabled => 1,
+    gpgcheck    => 0,
 }
-
-define add_yum_repo {
-    file { $name:
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '644',
-        path    => "/etc/yum.repos.d/${name}",
-        source  => "/vagrant/files/${name}",
-        purge   => true,
-    }
-}
-
-add_yum_repo { 
-    ['mozilla-services.repo', 'packages-mozilla.repo']: }
 
 package { $moz_packages:
     ensure  => present,
-    require => [File['mozilla-services.repo'], File['packages-mozilla.repo']]
+    require => [Yumrepo['mozilla-services'], Yumrepo['packages-mozilla']]
 }
 
 file { 'logstash.conf':
